cbda6ebf79702382dfd73758a7dce78e8c03a5bd
SMN947 <53320639+SMN947@users.noreply.github.com>
2020-10-31 04:53:08 +0000

    Create server.js

---
+++ back/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const port = 80;
+const app = express()
+const http = require("http").Server(app);
+var io = require('socket.io')(http);
+var socketUsers = require('socket.io.users');
+
+app.use(express.static(path.join(__dirname, '../')));
+
+var serverData = {};
+
+app.listen(port, function () {	
+    console.log( properties.ENV, ': Listening on port', port, '- start:', Date(Date.now()).toString()) ;	
+});
+
+io.on('connection', function (socket) {
+    socket.emit('serverData', serverData);
+    socket.on('new-user', (data) => {
+        serverData[socket.id] = data;
+        io.sockets.emit('serverData', serverData);
+    });
+    
+    socket.on("disconnect", () => {
+        delete serverData[socket.id]
+        io.sockets.emit('serverData', serverData);
+        delete serverData[socket.id];
+    });
+});
